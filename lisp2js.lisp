d1dfa437b2040f22ca9ff1dd008f76a05b9f8d97
Pau Fernández <pau.fernandez@upc.edu>
2011-05-18 23:16:06 +0200

    Changed output to stdout instead of file (could add '-o') + added comments

--- lisp2js.lisp
+++ lisp2js.lisp
@@ -1,5 +1,6 @@
 #!/usr/bin/sbcl --script
 
+;; Stuff required to load parenscript
 (require :asdf)
 (labels ((personal-dir (x)
 			  (merge-pathnames x (user-homedir-pathname))))
@@ -8,14 +9,12 @@
 (asdf:oos 'asdf:load-op :parenscript :verbose nil)
 
 (defun js-filename (lisp-filename)
+  "Converts `test.lisp' into `test.js'"
   (let ((base (subseq lisp-filename 0 
 							 (position #\. lisp-filename))))
 	 (format nil "~a.js" base)))
 
 (when (> (length sb-ext:*posix-argv*) 1)
   (let* ((input-filename (nth 1 sb-ext:*posix-argv*))
-			(js-filename (js-filename input-filename)))
-	 (with-open-file (parenscript:*parenscript-stream* js-filename 
-																		:direction :output
-																		:if-exists :supersede)
-		(parenscript:ps-compile-file (nth 1 sb-ext:*posix-argv*)))))
+			(parenscript:*parenscript-stream* *standard-output*))
+	 (parenscript:ps-compile-file input-filename)))
